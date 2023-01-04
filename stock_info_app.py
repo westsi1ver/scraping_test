b278663bcbc29f192963da38b702a72e532bfef4
seoeun park <87380941+westsi1ver@users.noreply.github.com>
2023-01-04 12:41:42 +0900

    Update stock_info_app.py

--- stock_info_app.py
+++ stock_info_app.py
@@ -11,7 +11,7 @@ from io import BytesIO
 import platform
 from matplotlib import font_manager, rc
 plt.rcParams['axes.unicode_minus'] = False
-if platform.system() == 'Linux':
+if platform.system() == 'Windows':
     rc('font', family='NanumGothic')
     
 #----------------------------------------
